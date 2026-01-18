e6de8ae272d26105f34f07910de32624cce55309
Zylang <joninurka7@gmail.com>
2026-01-18 22:41:18 +0100

    updated the kick message to inform about the beta

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").LocalPlayer:Kick("script it's down right now, soon gonna update it whit spectre v2, more info on my discord server");
+game:GetService("Players").LocalPlayer:Kick("script it's down right now, spectrev2 it's on beta on the discord server make a ticket to be a tester");
